f48f9bc76427d00412d6db87e75aeb372ad27735
wabbi-khan <kwahab789@gmail.com>
2023-02-13 17:06:15 +0500

    connecting mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app = require('./app');
 const dotenv = require('dotenv');
-const { path } = require('./app');
+const connectDatabase = require('./config/database');
+// const { path } = require('./app');
 
 // config
 dotenv.config({ path: 'backend/config/config.env' });
+
+//connecting to database
+connectDatabase();
+
 app.listen(process.env.PORT, () => {
   console.log(`Server in wordking on http://localhost:${process.env.PORT}`);
 });
