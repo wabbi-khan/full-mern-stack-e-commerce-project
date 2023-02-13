02f9bc049e57ae2ce011c976138badc132ffe752
wabbi-khan <kwahab789@gmail.com>
2023-02-13 18:02:27 +0500

    creating product api

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,14 @@
+const Product = require('../models/productModel');
+
+// creeate product
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    succes: true,
+    product,
+  });
+};
+
 exports.getAllProducts = (req, res) => {
   res.status(200).json({ message: 'Route is working fine' });
 };
---
+++ backend/models/productModel.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please Enter Product Name'],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please Enter Product Description'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please Enter Product Price'],
+    maxLength: [8, 'Price can not exceed 8 characters'],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, 'Please Enter Product Category'],
+  },
+  stock: {
+    type: String,
+    required: [true, 'Please Enter Product Stock'],
+    maxLength: [4, 'Price can not exceed 8 characters'],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model('Product', ProductSchema);
