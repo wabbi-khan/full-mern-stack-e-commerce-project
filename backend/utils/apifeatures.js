d6cc154f6fadcd4cedef5b16540d300d5dfe5e01
wabbi-khan <kwahab789@gmail.com>
2023-02-21 03:09:58 +0500

    pagination

--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -37,6 +37,12 @@ class ApiFeatures {
   }
 
   // pagination
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resultPerPage * (currentPage - 1);
+    this.query = this.query.limit(resultPerPage).skip(skip);
+    return this;
+  }
 }
 
 module.exports = ApiFeatures;
