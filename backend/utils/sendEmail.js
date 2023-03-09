475eb126b2c63786dd74b71229811e304647539a
wabbi-khan <kwahab789@gmail.com>
2023-03-10 04:30:36 +0500

    send mail to mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,13 +2,13 @@ const nodeMailer = require('nodemailer');
 
 const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
     service: process.env.SMPT_SERVICE,
     auth: {
       user: process.env.SMPT_MAIL,
       pass: process.env.SMPT_PASSWORD,
     },
-    // host: process.env.SMPT_HOST,
-    // port: process.env.SMPT_PORT,
     // service: process.env.SMPT_SERVICE,
     // auth: {
     //   user: process.env.SMPT_MAIL,
