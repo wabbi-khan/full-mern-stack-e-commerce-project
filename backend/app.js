9a3383518a2703f5478fdba75002eb39bd895fdf
wabbi-khan <kwahab789@gmail.com>
2023-04-29 02:18:42 -0700

    creating order api

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const errorMiddleware = require('./middleware/error');
-const cookieParser = require('cookie-parser');
+const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(cookieParser());
 // Import Routes
-const product = require('./routes/productRoute');
-const user = require('./routes/userRoute');
-app.use('/api/v1', product);
-app.use('/api/v1', user);
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 // middleware for error
 app.use(errorMiddleware);
