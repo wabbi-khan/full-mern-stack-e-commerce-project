d09962148ee2ef448d456d510d7d0a25b0e43c39
wabbi-khan <kwahab789@gmail.com>
2023-05-14 14:50:36 -0700

    install imp react library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+// import "./App.css";
 
-function App() {
-  return <div>ads</div>;
-}
+// function App() {
+//   return <div>ads</div>;
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
